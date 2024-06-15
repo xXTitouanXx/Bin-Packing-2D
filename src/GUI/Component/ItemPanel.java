3b69deaaa686b33c71479a1e307334c4c4926387
xXTitouanXx <17titou@gmail.com>
2024-06-15 23:11:29 +0200

    redimensionne les items si ca depasse le panel

--- src/GUI/Component/ItemPanel.java
+++ src/GUI/Component/ItemPanel.java
@@ -16,31 +16,73 @@ public class ItemPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        int panelWidth = getParent().getSize().width;
+        int panelHeight = getParent().getSize().height;
+
+        // Calculate the total height required
+        int totalHeightRequired = calculateTotalHeightRequired(panelWidth);
+
+        // Calculate scaling factor
+        double scalingFactor = 1.0;
+        if (totalHeightRequired > panelHeight) {
+            scalingFactor = (double) panelHeight / totalHeightRequired;
+        }
+
         int x = 5;
         int y = 5;
         int yMax = Integer.MIN_VALUE;
+        int i = 0;
+        int nb = 0;
+
         for (Item item : items) {
-            drawItem(g, item, x, y);
-            if (x + item.getWidth() < getParent().getSize().width) {
-                yMax = Math.max(yMax, item.getHeight());
-                //System.out.println("Valeur ymax : " + yMax);
-                x += item.getWidth() + 5;
-            } else {
+            int itemWidth = (int) (item.getWidth() * scalingFactor);
+            int itemHeight = (int) (item.getHeight() * scalingFactor);
+            nb++;
+
+            if (x + itemWidth + 5 > panelWidth) {
                 x = 5;
                 y += yMax + 5;
                 yMax = 0;
             }
-        }
-    }
 
-    private void drawItem(Graphics g, Item item, int x, int y) {
-        for (int row = 0; row < item.getHeight(); row++) {
-            for (int col = 0; col < item.getWidth(); col++) {
-                g.setColor(item.getColor());
-                g.drawRect(x + col, y + row, 1, 1);
+            // Check if the item can be drawn within the panel height
+            if (y + itemHeight + 5 <= panelHeight) {
+                drawItem(g, item, x, y, itemWidth, itemHeight);
+                yMax = Math.max(yMax, itemHeight);
+                x += itemWidth + 5;
+                i++;
+            } else {
+                // Break if we can't draw more items within the panel height
+                break;
             }
         }
 
+        System.out.println("Nb Item dessiné: " + i + ", nb iteration dans la boucle item: " + nb);
+    }
+
+    private int calculateTotalHeightRequired(int panelWidth) {
+        int x = 5;
+        int y = 5;
+        int yMax = Integer.MIN_VALUE;
+        int totalHeight = 0;
+
+        for (Item item : items) {
+            if (x + item.getWidth() < panelWidth) {
+                yMax = Math.max(yMax, item.getHeight());
+                x += item.getWidth() + 5;
+            } else {
+                x = 5;
+                totalHeight += yMax + 5;
+                yMax = item.getHeight();
+            }
+        }
+        totalHeight += yMax + 5; // Add the height of the last row
+        return totalHeight;
+    }
+
+    private void drawItem(Graphics g, Item item, int x, int y, int width, int height) {
+        g.setColor(item.getColor());
+        g.fillRect(x, y, width, height);
     }
 
     public void clearItems() {
@@ -50,5 +92,6 @@ public class ItemPanel extends JPanel {
 
     public void setItems(List<Item> items) {
         this.items = items;
+        repaint(); // Redessine le composant pour refléter les changements
     }
 }
